d88a2785ede244446c385fec64d8e3ea811b462b
MFort1906 <miguelgasilveira211@gmail.com>
2025-07-22 14:40:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ with gr.Blocks(title="W.S.T.B.R 2000", theme=gr.themes.Soft()) as demo:
             status = gr.Textbox(label="📌 Status do processo", interactive=False)
             arquivos = gr.File(label="📎 Arquivos traduzidos (.docx)", file_types=[".docx"], file_count="multiple")
 
-            btn.click(fn=rodar_interface, inputs=[pais_dropdown, qtd], outputs=[status, arquivos, app_box])
+            btn.click(fn=rodar_interface, inputs=[pais_dropdown, qtd], outputs=[status, arquivos])
 
     btn_login.click(fn=checar_senha, inputs=senha, outputs=[login_box, app_box])
 
