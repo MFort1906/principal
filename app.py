24fc360fb5a18a845906e8f073de11320603bd8f
MFort1906 <miguelgasilveira211@gmail.com>
2026-06-03 11:45:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-rom flask import Flask, render_template, request, jsonify, send_file, session, redirect, url_for
+from flask import Flask, render_template, request, jsonify, send_file, session, redirect, url_for
 import os
 import asyncio
 import json
